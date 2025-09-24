8ad24480bbd30548af291bf82b6792247c8106f6
uysolanki <uysolanki@gmail.com>
2025-09-24 08:59:10 +0530

    rps day1

--- javascriptprograms/data/main1.js
+++ javascriptprograms/data/main1.js
@@ -1,6 +1,17 @@
 import captain,{batsmen as bat,ipl} from './test.js'
+import products from './products.js'
+
 
 console.log(`I like ${bat}`)
 console.log(`My Fav Bowler is ${ipl.bowler}`)
 
 console.log(`Captain ${captain}`)
+
+let allCategories=products.map(
+    (product)=>(product.category)
+)
+
+console.log(allCategories)
+
+let uniqueCategories=new Set(allCategories)
+console.log(uniqueCategories)
