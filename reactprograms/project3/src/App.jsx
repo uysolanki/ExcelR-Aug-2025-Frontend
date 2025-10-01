dca965290582455f678c1124327ccafb51acdf19
uysolanki <uysolanki@gmail.com>
2025-10-01 10:23:40 +0530

    react project 3

---
+++ reactprograms/project3/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Excelr from './components/Excelr'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <div>
+      <Excelr/>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
---
+++ reactprograms/project3/src/components/Excelr.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Excelr = () => {
+    let n1=10;
+    let n2=20;
+    let n3=n1+n2;
+    console.log(n3)
+  return (
+    <div>
+            <h1> Welcome to ExcelR</h1>
+            <p>Java Springboot and React JS</p>
+            <h2>{n3}</h2>
+    </div>
+  )
+}
+
+export default Excelr
