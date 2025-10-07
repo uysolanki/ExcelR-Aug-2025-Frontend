98d69c4cd2156552bb4dad460f6d1e258ece22e8
uysolanki <uysolanki@gmail.com>
2025-10-07 08:30:42 +0530

    rich text box

--- reactprograms/project4/src/components/RichTextBox.jsx
+++ reactprograms/project4/src/components/RichTextBox.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import './RichTextBox.css'
+import Button from './Button'
 
 const RichTextBox = () => {
   let [data,setData]=useState('')
@@ -21,17 +22,27 @@ const RichTextBox = () => {
   {
     setPreviewData(previewData.toLowerCase())
   }
+
+  function reset()
+  {
+    setData('')
+    setPreviewData('')
+  }
   return (
     <>
       <div className='parent-container'>
         <div>
-        <textarea rows='12' cols='30' value={data} onChange={handleOnChange}></textarea>
+        <textarea rows='12' cols='30' value={data} onChange={handleOnChange} placeholder="Enter your text here..."></textarea>
           <div>
-            <button onClick={convertToUpperCase}>Upper Case</button>
-            <button onClick={convertToLowerCase}>Lower Case</button>
+            {/* <button onClick={convertToUpperCase}>Upper Case</button> */}
+            <Button handleOnClick={convertToUpperCase} text='Upper Case'/>
+            {/* <button onClick={convertToLowerCase}>Lower Case</button>
+            <button onClick={reset}>Reset</button> */}
+            <Button handleOnClick={convertToLowerCase} text='Lower Case'/>
+            <Button handleOnClick={reset} text='RESET'/>
             <p>Char Count : {previewData.length}</p>
-            <p>Word Count : {previewData.trim().split(' ').length}</p>
-            <p>Reading Time : {parseInt(previewData.trim().split(' ').length)/100 + " Minutes"}</p>
+            <p>Word Count :{previewData ? previewData.trim().split(/\s+/).length : 0}</p>
+            <p>Reading Time : {previewData ? parseInt(previewData.trim().split(' ').length)/100 + " Minutes" : "0 Minutes"}</p>
           </div>
         </div>
 
